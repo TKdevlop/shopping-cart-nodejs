c7fedf3837a758a96a904e8f1b72ed7a00783bc2
TKdevlop <34345518+TKdevlop@users.noreply.github.com>
2018-07-22 14:25:20 +0530

    final commit

--- bin/config.js
+++ bin/config.js
@@ -1,8 +1,8 @@
 const credentails = {
-    username:"tushar",//"Enter your mlabs username",
-    password:"9411tushar"//"enter your mlabs password"
+    username:"",//"Enter your mlabs username",
+    password:""//"enter your mlabs password"
 }
-const stripeKey ="sk_test_JIrCfVj7bJ3N3qAYunNHDX2N"
+const stripeKey =""//sk key
 
 module.exports ={
     credentails,
